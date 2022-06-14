74a92bb7a3efeb301f990bccea2eb9d74efe0d20
Reza Vilera <vilera_reza99@yahoo.com>
2022-06-14 22:39:49 +0700

    test

--- capture.py
+++ capture.py
@@ -2,19 +2,13 @@ import cv2 as cv
 import numpy as np
 import pickle
 import subprocess
+import time
 
 fileName = 'capture_undistorted.png'
 imgDir = "img_raw"
 outDir = "img_corrected"
 
 # Loading calibration data
-try:
-    #camera_cal = pickle.load(open('../camera_cal.p',"rb"))
-    camera_cal = pickle.load(open('cal_data.p',"rb"))
-    mtx = camera_cal["mtx"]
-    dist = camera_cal["dist"]
-except:
-    print ('Calibration data does not exist')
 
 def capture(outDir, fileName, mtx, dist):
     subprocess.run(['libcamera-still', '--denoise', 'off', '--shutter', '70000', '--gain', '0', '--awb', 'cloudy', '--immediate', '--rawfull', '-e', 'png', '-o', f'{outDir}/{fileName}'])
@@ -26,4 +20,16 @@ def capture(outDir, fileName, mtx, dist):
     except:
         print ('Image not found')
 
+
+t1 = time.time()
+try:
+    camera_cal = pickle.load(open('../camera_cal.p',"rb"))
+    #camera_cal = pickle.load(open('cal_data.p',"rb"))
+    mtx = camera_cal["mtx"]
+    dist = camera_cal["dist"]
+except:
+    print ('Calibration data does not exist')
+
 capture(outDir, fileName, mtx, dist)
+t2 = time.time()
+print (f'Timelapse: {t2-t1}')
